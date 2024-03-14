5228ae529c16f46d762e2a4a04ec3f09c12b7bbd
Nuru Dashdamirli <nurutasdemir@gmail.com>
2024-03-15 00:09:39 +0400

    Update AssemblyInfo.cs
    To prevent the application from being falsely detected as a virus

--- BinaryStringDetector/Properties/AssemblyInfo.cs
+++ BinaryStringDetector/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HEXStringDetector")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BinaryStringDetector")]
+[assembly: AssemblyDescription("Tool to find strings in binaries")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HEXStringDetector")]
+[assembly: AssemblyCompany("NTD")]
+[assembly: AssemblyProduct("BinaryStringDetector")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
